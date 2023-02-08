4a95a0e9d288677f01ec1603e343a0088e637212
pimonov-student <a.pimonov@g.nsu.ru>
2023-02-08 18:08:14 +0700

    Короче пересел с YouTube на ХабрЭ, там как-то с основ объясняют, но мб буду параллельно и там, и там смотреть

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,52 @@
 
 int main(void)
 {
-    GLFWwindow* window;
+    // Инициализация GLFW
+    glfwInit();
+    // Настройка GLFW
+    // Минимальная необходимая версия OpenGL (мажорная и минорная)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    // "Установка профайла, для которого создается контекст"
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Запрет на динамическое изменение окна
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    // Создаем объект окна
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
+    // Задаем window рабочим окном
     glfwMakeContextCurrent(window);
-	
-	if (!gladLoadGL())
-	{
-		std::cout << "Can't load GLAD" << std::endl;
-		return -1;
-	}
-	
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
-	
-	glClearColor(1, 1, 0, 1);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    // Загружаем библиотеку GLAD (OpenGL)
+    if (!gladLoadGL())
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::cout << "Can't load GLAD" << std::endl;
+        return -1;
     }
 
+    // Передаем OpenGL размер window
+    int width, heigth;
+    glfwGetFramebufferSize(window, &width, &heigth);
+
+    glViewport(0, 0, width, heigth);
+
+    // Цикл отрисовки окна, в условии функция... ну очевидно, что делает
+    while (!glfwWindowShouldClose(window))
+    {
+        // Проверка на наличие всяких событий
+        glfwPollEvents();
+        // Замена буфера с цветами для каждого пикселя в window
+        glfwSwapBuffers(window);
+    }
+
+    // После завершения цикла, закрывавем окно
     glfwTerminate();
+
     return 0;
 }
