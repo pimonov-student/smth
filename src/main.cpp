67da8c2346ccfe07eb6afe3855db26e3a0cfbabc
pimonov-student <a.pimonov@g.nsu.ru>
2023-02-08 18:50:51 +0700

    Код для первого урока с Хабра

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 
 #include <iostream>
 
+// Функция "обратного вызова", которая отслеживает нажатие ESC и закрывает окно
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
     // Инициализация GLFW
@@ -26,6 +35,8 @@ int main(void)
     }
     // Задаем window рабочим окном
     glfwMakeContextCurrent(window);
+    // Передаем нашу рукописную callback-функцию в GLFW функцию проверки таких callback функций
+    glfwSetKeyCallback(window, key_callback);
 
     // Загружаем библиотеку GLAD (OpenGL)
     if (!gladLoadGL())
@@ -40,15 +51,22 @@ int main(void)
 
     glViewport(0, 0, width, heigth);
 
+    // Устанавливаем цвет, которым будем зачищать окно
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
     // Цикл отрисовки окна, в условии функция... ну очевидно, что делает
     while (!glfwWindowShouldClose(window))
     {
         // Проверка на наличие всяких событий
         glfwPollEvents();
+
+        // Зацищаем окно
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // Замена буфера с цветами для каждого пикселя в window
         glfwSwapBuffers(window);
     }
-
+ 
     // После завершения цикла, закрывавем окно
     glfwTerminate();
 
